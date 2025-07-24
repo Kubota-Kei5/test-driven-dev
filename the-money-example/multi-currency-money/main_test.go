cf53e16f5ed11efb46c1bc283b079b7c2ba253d0
kubotake-desktop <keigo1003560@gmail.com>
2025-07-25 08:50:13 +0900

    サブテストの動作確認

--- the-money-example/multi-currency-money/main_test.go
+++ the-money-example/multi-currency-money/main_test.go
@@ -7,19 +7,18 @@ import (
 )
 
 // func TestMoneyMultiplier1(t *testing.T) {
-//     assert.Equal(t, MoneyMultiplier(0, 7), 0,  "0 * 7 should equal 0")
+//     t.Run("Real Number Multiplication", func(t *testing.T) {
+//         assert.Equal(t, MoneyMultiplier(2, 5), 10,  "2 * 5 should equal 10")
+//     })
+//     t.Run("Zero Multiplication", func(t *testing.T) {
+//         assert.Equal(t, MoneyMultiplier(0, 7), 0,  "0 * 7 should equal 0")
+//     })
 // }
-
-// func TestMoneyMultiplier2(t *testing.T) {
-//     assert.Equal(t, MoneyMultiplier(2, 5), 10,  "2 * 5 should equal 10")
-// }
-
 
 func TestMoneyMultiplier1(t *testing.T) {
-    t.Run("Real Number Multiplication", func(t *testing.T) {
-        assert.Equal(t, MoneyMultiplier(2, 5), 10,  "2 * 5 should equal 10")
-    })
-    t.Run("Zero Multiplication", func(t *testing.T) {
-        assert.Equal(t, MoneyMultiplier(0, 7), 0,  "0 * 7 should equal 0")
-    })
+    assert.Equal(t, MoneyMultiplier(0, 7), 0,  "0 * 7 should equal 0")
+}
+
+func TestMoneyMultiplier2(t *testing.T) {
+    assert.Equal(t, MoneyMultiplier(2, 5), 10,  "2 * 5 should equal 10")
 }
